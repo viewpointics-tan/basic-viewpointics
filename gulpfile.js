c2cf6b3da814c8e661b2eca88d2d265bad3cf9ac
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-01-08 17:18:24 +0900

    gulpfile: シェルコマンドのテスト

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
+const glob = require('glob');
+const exec = require('child_process').exec;
+
 // npx gulp build
 function build(cb) {
-  // place code for your default task here
-  console.log("test");
 
-  cb();  // タスクの最後に必要
+  glob('src/**/*.md', (err, files) => {
+    for (const file of files) {
+      exec("echo " + file, (err, stdout, stderr) => {
+        cb(err);
+        console.log(stdout);
+      })
+    }
+  })
+
+  // const command = "pandoc --version";
+  // exec(command, (err, stdout, stderr) => {
+  //   console.log(stdout);
+  //   cb(err);
+  // })
+
+  cb();  // タスクの最後に必要（正常終了）
 }
 
 // npx gulp hoge で実行可能 (exports.hoge に対して)
